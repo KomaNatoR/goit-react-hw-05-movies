0eee3275ccdf6ab43b0f451a71cdedc64cd57c86
Serhii Markov <goo.komalel@gmail.com>
2023-02-12 10:43:36 +0200

    Menu
    add nanoid, rewrited NavBar to map(items)

--- src/components/modules/Navbar/Navbar.jsx
+++ src/components/modules/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
 import { NavLink } from "react-router-dom";
 
+import { items } from "./items";
+
 import { Header } from "./navbar.styled";
 
 const Navbar = () => {
+    const eles = items.map(({ id, to, text }) => (
+        <li key={id}>
+            <NavLink to={to}>{text}</NavLink>
+        </li>)
+    );
+
     return (
         <Header>
             <ul>
-                <li>
+                {eles}
+                {/* <li>
                     <NavLink to={'/'}>Home</NavLink>
                 </li>
                 <li>
                     <NavLink to={'/movie'}>Movies</NavLink>
-                </li>
+                </li> */}
             </ul>
         </Header>
     );
