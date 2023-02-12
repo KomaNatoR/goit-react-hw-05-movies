0b35a505fd500346d75b1f026f36f6db1a77eb10
Serhii Markov <goo.komalel@gmail.com>
2023-02-12 15:59:29 +0200

    useSearchParams
    changed some useState to useSearchParams onto Movie-page

--- src/components/pages/Movies/Movies.jsx
+++ src/components/pages/Movies/Movies.jsx
@@ -1,28 +1,28 @@
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import axios from "axios";
 
 import PostSearchForm from "components/modules/PostSearchForm/PostSearchForm";
 import PostList from "components/modules/PostList/PostList";
 
 const Movies = () => {
-    const [filmName, setFilmName] = useState('');
     const [filmsArr, setFilmsArr] = useState([]);
 
-    // const [searcParams, setSearchParam] = useSearchParams;
-    // const search = searcParams.get('query');
+    const [searchParams, setSearchParam] = useSearchParams();// searchParams це як стейт!
+    const search = searchParams.get('query'); // тут пишеться назва параметру!
+    console.log(search);
 
     useEffect(() => {
         const KEY = '7456877804751c2ee672618d82b01711';
-        const URL = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&page=1&include_adult=false&query=${filmName}`;
+        const URL = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
 
         axios.get(URL).then(fetchFilms);
         axios.get(URL).then(resp=>console.log(resp));    
-    }, [filmName]);
+    }, [search]);
     const fetchFilms = ({ data }) => setFilmsArr(data.results);
 
-    const onSearch = ({search}) => setFilmName(search);
+    const onSearch = ({search}) => setSearchParam({query:search}); // встановлення searchParams, можна через кому декілька штук!
     
-
 
     return (
         <>
